f9660c344a8faef692da64bbe4a1f5f64a299c7e
Taavi Rehemägi <taavirehemagi@gmail.com>
2018-05-17 01:19:42 +0300

    subscribing one log group to kinesis stream

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 'use strict'
 
+const AWS = require('aws-sdk')
+const Logs = new AWS.CloudWatchLogs()
+const promisify = require('util').promisify
+const putSubscriptionFilter = promisify(Logs.putSubscriptionFilter)
+
 exports.handler = async function (event, context) {
-   console.log('testing');
-   return 'something';
+
+  // get log groups to subscribe to.
+ 
+  const params = {
+    destinationArn: 'arn:aws:firehose:us-east-1:458024764010:deliverystream/kinesis-consumer-us-east-1',
+		filterName: 'dashbird-streamer-random',
+		filterPattern: '-END',
+		logGroupName: 'STRING_VALUE',
+		distribution: 'ByLogStream'
+  }
+
+  await putSubscriptionFilter(params)
+  console.log('subscribed to stream')
+  return true;
 } 
