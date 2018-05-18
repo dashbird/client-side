3806956d339b826c8c147df3ff8ed587fe3892af
Taavi Rehemägi <taavirehemagi@gmail.com>
2018-05-18 18:28:05 +0300

    use the targets provided

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 'use strict'
 
 const AWS = require('aws-sdk')
-const Logs = new AWS.CloudWatchLogs()
+const CWLogs = new AWS.CloudWatchLogs()
 const promisify = require('util').promisify
-const putSubscriptionFilter = promisify(Logs.putSubscriptionFilter)
+const putSubscriptionFilter = promisify(CWLogs.putSubscriptionFilter)
 const request = require('request-promise')
-
-const DESTINATION_ARN = 'arn:aws:firehose:us-east-1:458024764010:deliverystream/kinesis-consumer-us-east-1'
 
 exports.handler = async function (event, context) {
 
-  // get log groups to subscribe to.
   const client = await request('https://mkhjm850ze.execute-api.us-east-1.amazonaws.com/prod/status?externalId=123')
 
   if(client.status === 'enabled') {
     for (lambda of client.lambdas) {
       await putSubscriptionFilter({
-        destinationArn: DESTINATION_ARN,
+        destinationArn: lambda.destination,
         filterName: `dashbird-streamer-${lambda.name}`,
         filterPattern: '-END',
-        logGroupName: lambda.logGroup,
+        logGroupName: lambda.name,
         distribution: 'ByLogStream'
       })
       console.log(`subscribed ${lambda.name} to kinesis stream`)
