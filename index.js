aa8e98c2b5d0539310c7513b5a2b57702e620fe7
ReigoHein <reigo@clanbeat.com>
2018-05-24 10:41:49 +0300

    Added error reporting to subscriber

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ exports.handler = async function (event, context) {
 
   if (client.status.toLowerCase() === 'active') {
     let regionGroups = _.groupBy(client.observables, (o) => o.region)
+    console.log(`Client is active, processing ${client.observables.length} observables.`)
 
     promises = _.map(regionGroups, async (logGroups, region) => {
       let CWLogs = new AWS.CloudWatchLogs({
@@ -32,12 +33,12 @@ exports.handler = async function (event, context) {
     promises.push(updateRoleArn())
   }
 
-  await Promise.all(promises)
+  Promise.all(promises)
 
   return true
 }
 
-async function updateRoleArn () {
+async function updateRoleArn() {
   return request({
     method: 'POST',
     uri: `${BASE_URL}/${externalId}/delegation`,
@@ -48,39 +49,57 @@ async function updateRoleArn () {
   })
 }
 
-async function processLogGroups (client, groups) {
+async function processLogGroups(client, groups) {
   const requests = _.map(groups, (observable) => {
     if (observable.action.toLowerCase() === 'upsert') {
+      console.log(`Upserting`, observable)
       return upsertObservable(client, observable)
     } else {
+      console.log(`Removing`, observable)
       return removeObservable(client, observable)
     }
   })
   return Promise.all(requests)
 }
 
-async function upsertObservable (client, observable) {
-  try {
-    let existingFilters = await client.describeSubscriptionFilters({
-      logGroupName: observable.logGroup
-    }).promise()
+async function upsertObservable(client, observable) {
+  return client.describeSubscriptionFilters({
+    logGroupName: observable.logGroup
+  }).promise().then((existingFilters) => {
     if (existingFilters.subscriptionFilters.length > 0) {
       let isDashbirdFilter = _.some(existingFilters.subscriptionFilters, (filter) => filter.destinationArn.indexOf('458024764010') !== -1)
       if (isDashbirdFilter) {
         return putObservable(client, observable)
+      } else {
+        console.log(`Log group ${observable.logGroup} has a subscription filter belonging to someone else, skipping.`)
       }
     } else {
       return putObservable(client, observable)
     }
-  } catch (ex) {
-    // Ignore ResourceNotFoundException
-    if (ex.code !== 'ResourceNotFoundException') {
-      console.log(`Could not import extId ${externalId} logGroup ${observable.logGroup} with exception ${ex.toString()}`)
-    }
-  }
+  }, (err) => {
+    return logGroupError(err, observable);
+  })
 }
 
-async function putObservable (client, observable) {
+async function logGroupError(err, observable) {
+  console.log(`Reporting error`, err.code, `\nwith observable`, observable);
+  let body = {
+    action: observable.action,
+    status: 'FAIL',
+    error: err.code === 'ResourceNotFoundException' ? err.code : null
+  }
+
+  return request({
+    method: 'POST',
+    uri: `${BASE_URL}/${externalId}/loggroup/${observable.id}`,
+    json: true,
+    body: body
+  })
+}
+
+async function putObservable(client, observable) {
+  console.log(`Adding subscription filter to log group ${observable.logGroup} with filter name ${observable.region}`)
+
   return client.putSubscriptionFilter({
     destinationArn: observable.destination,
     filterName: observable.region,
@@ -93,15 +112,21 @@ async function putObservable (client, observable) {
       uri: `${BASE_URL}/${externalId}/loggroup/${observable.id}`,
       json: true,
       body: {
-        success: true
+        action: observable.action,
+        status: 'SUCCESS',
+        error: null
       }
     })
   })
 }
 
-async function removeObservable (client, observable) {
+async function removeObservable(client, observable) {
+  console.log(`Removing log group ${observable.logGroup} with filter name ${observable.region}`)
+
   return client.deleteSubscriptionFilter({
     filterName: observable.region,
     logGroupName: observable.logGroup
-  }).promise()
+  }).promise().catch((err) => {
+    return logGroupError(err, observable)
+  });
 }
